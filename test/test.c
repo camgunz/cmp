96ccda6f233fe570950b1740ff36a0a9ceb0ed37
camgunz <charles.gunyon@gmail.com>
2014-04-29 18:06:47 -0400

    Added fixed integer tests

--- test/test.c
+++ test/test.c
@@ -1,7 +1,9 @@
+#include <inttypes.h>
+#include <stdarg.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <inttypes.h>
+#include <string.h>
 
 #include "utils.h"
 #include "buf.h"
@@ -9,15 +11,18 @@
 #include <stdbool.h>
 #include "cmp.h"
 
-#define run_test(t)             \
-  printf(#t " test: ");         \
-  if (!run_ ## t ## _tests()) { \
-    printf("-- FAILED --\n");   \
-    return EXIT_FAILURE;        \
-  }                             \
-  else {                        \
-    printf("passed\n");         \
+#define run_test(t)                     \
+  printf(#t " test: ");                 \
+  if (!run_ ## t ## _tests()) {         \
+    printf("-- FAILED --\n");           \
+    printf("\t %s\n", failure_message); \
+    return EXIT_FAILURE;                \
+  }                                     \
+  else {                                \
+    printf("passed\n");                 \
   }
+
+static char failure_message[4096];
 
 static bool buf_reader(cmp_ctx_t *ctx, void *data, size_t limit) {
   buf_t *buf = (buf_t *)ctx->buf;
@@ -39,6 +44,16 @@ static void setup_cmp_and_buf(cmp_ctx_t *cmp, buf_t *buf) {
   cmp_init(cmp, buf, &buf_reader, &buf_writer);
 }
 
+static void set_error(const char *msg, ...) {
+  va_list args;
+
+  va_start(args, msg);
+
+  vsprintf(failure_message, msg, args);
+
+  va_end(args);
+}
+
 bool run_msgpack_tests(void) {
   buf_t in_buf, out_buf;
   cmp_ctx_t in_cmp, out_cmp;
@@ -54,11 +69,11 @@ bool run_msgpack_tests(void) {
 
   while (M_BufferGetCursor(&in_buf) < (M_BufferGetSize(&in_buf))) {
     if (!cmp_read_object(&in_cmp, &obj)) {
-      fprintf(stderr, "Error reading object: %s\n", cmp_strerror(&in_cmp));
+      set_error("Error reading object: %s\n", cmp_strerror(&in_cmp));
       return false;
     }
     if (!cmp_write_object(&out_cmp, &obj)) {
-      fprintf(stderr, "Error writing object: %s\n", cmp_strerror(&out_cmp));
+      set_error("Error writing object: %s\n", cmp_strerror(&out_cmp));
       return false;
     }
   }
@@ -70,20 +85,122 @@ bool run_msgpack_tests(void) {
   );
 
   if (!buffers_equal) {
+    set_error("Buffers did not match.\n");
     M_BufferPrint(&in_buf);
     M_BufferPrint(&out_buf);
+    M_BufferFree(&in_buf);
+    M_BufferFree(&out_buf);
+
+    return false;
+  }
+
+  M_BufferFree(&in_buf);
+  M_BufferFree(&out_buf);
+
+  return true;
+}
+
+bool run_fixedint_tests(void) {
+  buf_t buf;
+  cmp_ctx_t cmp;
+
+  setup_cmp_and_buf(&cmp, &buf);
+
+  if (cmp_write_pfix(&cmp, 128)) {
+    set_error("Wrote a positive fixed integer that was too large.\n");
+    return false;
+  }
+  cmp.error = 0;
+
+  if (cmp_write_pfix(&cmp, 200)) {
+    set_error("Wrote a positive fixed integer that was too large.\n");
+    return false;
+  }
+  cmp.error = 0;
+
+  if (cmp_write_pfix(&cmp, -1)) {
+    set_error("Wrote a negative positive fixed integer (-1).\n");
+    return false;
+  }
+  cmp.error = 0;
+
+  if (cmp_write_pfix(&cmp, -31)) {
+    set_error("Wrote a negative positive fixed integer (-31).\n");
+    return false;
+  }
+  cmp.error = 0;
+
+  if (cmp_write_pfix(&cmp, -32)) {
+    set_error("Wrote a negative positive fixed integer (-32).\n");
+    return false;
+  }
+  cmp.error = 0;
+
+  if (cmp_write_pfix(&cmp, -127)) {
+    set_error("Wrote a negative positive fixed integer (-127).\n");
+    return false;
+  }
+  cmp.error = 0;
+
+  if (cmp_write_pfix(&cmp, -128)) {
+    set_error("Wrote a negative positive fixed integer (-128).\n");
+    return false;
+  }
+  cmp.error = 0;
+
+  if (cmp_write_nfix(&cmp, 0)) {
+    set_error("Wrote 0 as a negative fixed integer.\n");
+    return false;
+  }
+  cmp.error = 0;
+
+  if (cmp_write_nfix(&cmp, 1)) {
+    set_error("Wrote 1 as a negative fixed integer.\n");
+    return false;
+  }
+  cmp.error = 0;
+
+  if (cmp_write_nfix(&cmp, 128)) {
+    set_error("Wrote 128 as a negative fixed integer.\n");
+    return false;
+  }
+  cmp.error = 0;
+
+  if (cmp_write_nfix(&cmp, -33)) {
+    set_error("Wrote a negative fixed integer that was too large.\n");
+    return false;
+  }
+  cmp.error = 0;
+
+  if (!cmp_write_pfix(&cmp, 0)) {
+    set_error("Failed to write pfix (0): %s.\n", cmp_strerror(&cmp));
+    return false;
+  }
+
+  if (!cmp_write_pfix(&cmp, 1)) {
+    set_error("Failed to write pfix (1): %s.\n", cmp_strerror(&cmp));
+    return false;
+  }
+
+  if (!cmp_write_pfix(&cmp, 127)) {
+    set_error("Failed to write pfix (127): %s.\n", cmp_strerror(&cmp));
+    return false;
+  }
+
+  if (!cmp_write_nfix(&cmp, -1)) {
+    set_error("Failed to write nfix (-1): %s.\n", cmp_strerror(&cmp));
+    return false;
+  }
+
+  if (!cmp_write_nfix(&cmp, -32)) {
+    set_error("Failed to write nfix (-32): %s.\n", cmp_strerror(&cmp));
     return false;
   }
 
   return true;
 }
 
-bool run_fixedint_tests(void) {
-  return true;
-}
-
 int main(void) {
-
   run_test(msgpack);
   run_test(fixedint);
 
