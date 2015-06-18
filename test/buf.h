29bd2f7ac74064b0f909305466e1d7225e4ec1c4
Charlie Gunyon <charles.gunyon@gmail.com>
2015-06-18 10:06:06 -0400

    Put buf.h under the MIT license

--- test/buf.h
+++ test/buf.h
@@ -1,25 +1,26 @@
-/*****************************************************************************/
-/* D2K: A Doom Source Port for the 21st Century                              */
-/*                                                                           */
-/* Copyright (C) 2014: See COPYRIGHT file                                    */
-/*                                                                           */
-/* This file is part of D2K.                                                 */
-/*                                                                           */
-/* D2K is free software: you can redistribute it and/or modify it under the  */
-/* terms of the GNU General Public License as published by the Free Software */
-/* Foundation, either version 2 of the License, or (at your option) any      */
-/* later version.                                                            */
-/*                                                                           */
-/* D2K is distributed in the hope that it will be useful, but WITHOUT ANY    */
-/* WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS */
-/* FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more    */
-/* details.                                                                  */
-/*                                                                           */
-/* You should have received a copy of the GNU General Public License along   */
-/* with D2K.  If not, see <http://www.gnu.org/licenses/>.                    */
-/*                                                                           */
-/*****************************************************************************/
+/*
+The MIT License (MIT)
 
+Copyright (c) 2015 Charles Gunyon
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
 
 #ifndef M_BUF_H__
 #define M_BUF_H__
