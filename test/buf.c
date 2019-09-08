b8383e12810298fde6117c28637560df6f0c482d
Francois Perrad <francois.perrad@gadz.org>
2019-09-08 15:13:27 +0200

    remove useless initialization

--- test/buf.c
+++ test/buf.c
@@ -158,8 +158,8 @@ void M_BufferSetString(buf_t *buf, const char *data, size_t length) {
 }
 
 bool M_BufferSetFile(buf_t *buf, const char *filename) {
-  FILE *fp = NULL;
-  size_t length = 0;
+  FILE *fp;
+  size_t length;
   bool out = false;
 
   if ((fp = fopen(filename, "rb")) == NULL)
