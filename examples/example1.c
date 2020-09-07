a946ee48bbc2ca54715c8b9b587ae801317861c2
Francois Perrad <francois.perrad@gadz.org>
2020-09-07 09:09:16 +0200

    remove useless include

--- examples/example1.c
+++ examples/example1.c
@@ -22,8 +22,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-#include <stdbool.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
--- examples/example2.c
+++ examples/example2.c
@@ -24,8 +24,6 @@ THE SOFTWARE.
 
 #include <errno.h>
 #include <inttypes.h>
-#include <stdbool.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
